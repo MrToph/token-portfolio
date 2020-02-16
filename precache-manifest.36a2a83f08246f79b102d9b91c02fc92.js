bb57b77f12b191070846c7ab589b090f450ff95f
MrToph <MrToph@users.noreply.github.com>
2020-02-16 06:39:58 +0000

    deploy: 55b191cfed0b949256d6837222049ba1320a5432

---
+++ precache-manifest.36a2a83f08246f79b102d9b91c02fc92.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c0d5ea5e53d04cbd143f79332c5246d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3105fc3e381e03689185",
+    "url": "/static/js/2.1d4e7254.chunk.js"
+  },
+  {
+    "revision": "b3d4d81795d6aa19c778bff9ab4c5fb2",
+    "url": "/static/js/2.1d4e7254.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "473e2a5855bb7467043e",
+    "url": "/static/js/main.ebd247e0.chunk.js"
+  },
+  {
+    "revision": "70d372efa5f5d3923b97",
+    "url": "/static/js/runtime-main.0f8b1cd2.js"
+  }
+]);
